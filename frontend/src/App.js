8d662f18bbe690ecbe8a49471ca9e24cca340e78
salmonco <xkqjsslsek80@naver.com>
2023-02-01 15:18:26 +0900

    Add api test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const getData = async () => {
+    try {
+      const response = await fetch("/test");
+      const data = await response.json();
+
+      alert("GET", data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const postData = async () => {
+    try {
+      const response = await fetch("/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Test",
+          body: "I am testing!",
+          userId: 1,
+        }),
+      });
+      const data = await response.json();
+
+      alert("POST", data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    postData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
